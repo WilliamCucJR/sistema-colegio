832abda292c78d152288e084cba569e9d4009b93
WilliamCucJR <wcuc96dev@gmail.com>
2024-10-13 20:51:46 -0600

    Creación de tabla y formulario en mantenimiento de carreras

--- app/Views/mantenimiento_carreras.php
+++ app/Views/mantenimiento_carreras.php
@@ -2,8 +2,91 @@
 <?= $this->include('templates/sidebar'); ?>
 
 <main class="main">
-    <h1>Mantenimiento de carreras</h1>
+    <h2><i class="fa-solid fa-graduation-cap"></i>Carreras</h2>
+    <button type="button" class="btn modal-button" data-toggle="modal" data-target="#exampleModal">
+        <i class="fa-solid fa-plus"></i> Agregar
+    </button>
+    <div class="table-container">
+        <table class="styled-table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </main>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <!-- Modal header content -->
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="row">
+                        <div class="col-md-6 input-div">
+                            <label>Nombres</label>
+                            <input type="text" class="form-control" id="nombres" placeholder="Nombres">
+                        </div>
+                        <div class="col-md-6 input-div">
+                            <label>Apellidos</label>
+                            <input type="text" class="form-control" id="apellidos" placeholder="Apellidos">
+                        </div>
+                        <div class="col-md-6 input-div">
+                            <label>Fecha de nacimiento</label>
+                            <input type="date" class="form-control" id="fecha_nacimiento" placeholder="Nombres">
+                        </div>
+                        <div class="col-md-6 input-div">
+                            <label>Carrera</label>
+                            <select id="fk_carrera_id" class="form-control">
+                                <option selected>Seleccionar...</option>
+                                <option>...</option>
+                            </select>
+                        </div>
+                        <div class="col-md-6 input-div">
+                            <label for="exampleFormControlFile1">Archivo</label>
+                            <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary close-modal-button" data-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary modal-button">Guardar</button>
+            </div>
+        </div>
+    </div>
+</div>
 <nav class="navbar">
     <i class="fa-solid fa-bars" id="sidebar-close"></i>
 </nav>
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -7,7 +7,9 @@
     <title>Mi Proyecto</title>
     <link rel="stylesheet" href="<?= base_url('assets/css/main.css'); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= base_url('assets/css/sidebar.css'); ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/mantenimiento.css'); ?>">
 </head>
 
 <body>
